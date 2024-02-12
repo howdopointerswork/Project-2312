5339c3cbb925399144da4db5946740f494a4db33
howdopointerswork <131082492+howdopointerswork@users.noreply.github.com>
2024-02-12 10:22:33 +0000

    Added basic outline of GUI (no buttons yet). To run, open console/terminal; type: python3 GUI.py (ensure you have changed to the directory where the file is).

---
+++ GUI.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+
+
+
+
+root = Tk()
+
+#Layout
+root.title("Database GUI")
+root.geometry("600x600")
+root.resizable(0,0)
+
+
+#Colouring
+root['bg'] = "white"
+
+
+root.mainloop()
