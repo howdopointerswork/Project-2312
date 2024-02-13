a846468cfbe1fbed8a4396420b81f3e123316b36
howdopointerswork <131082492+howdopointerswork@users.noreply.github.com>
2024-02-13 03:53:11 +0000

    Testing Tkinter
    Testing button, placement, and frame functionality. Testing events associated with buttons.

--- GUI.py
+++ GUI.py
@@ -3,21 +3,42 @@ from tkinter import ttk
 
 
 
+#Functions here
+def changeText():
 
+  toChange.set("Button Pressed")
+
+
+
+
+#For root
 root = Tk()
 
-#Layout
 root.title("Database GUI")
 root.geometry("600x600")
 root.resizable(0,0)
-
-
-#Buttons to go here
-
-#Frames for pages here (uses lift())
-
-#Colouring
 root['bg'] = "white"
 
 
+
+#Main page
+main = Frame(root, height = 600, width = 600)
+
+main.pack()
+
+
+#Variables 
+toChange = StringVar()  
+
+
+
+#Widgets 
+TestLabel = ttk.Label(main, text = "This Will Change", textvariable = toChange).place(x = 250, y = 100)
+TestButton = ttk.Button(main, text="Do Something", command=changeText).place(x = 250, y = 200)
+
+
+#Additional actions
+toChange.set("Button Not Pressed")
+
+#Draw to Screen
 root.mainloop()
